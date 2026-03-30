d7d2e03015d9c34ee6db136f8e0cca5e757eae57
DaynerPow <daynerpowtube@gmail.com>
2026-03-30 19:39:01 +0300

    <commit 2>

--- Advance_1_DaynerPow/Program.cs
+++ Advance_1_DaynerPow/Program.cs
@@ -1,10 +1,5 @@
-﻿using Microsoft.SqlServer.Server;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
+﻿using System;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Advance_1_DaynerPow
 {
@@ -12,7 +7,7 @@ namespace Advance_1_DaynerPow
     {
         static void SummaryText(string text, double summary)
         {
-            Console.WriteLine(summary + "% " + text);
+            Console.WriteLine(summary.ToString("F2") + "% " + text);
         }
 
         static void ShowSeparator()
@@ -47,67 +42,67 @@ namespace Advance_1_DaynerPow
 
             /*try
             {
-                Write("Вкажіть вашу ціль по крокам (Тільки цифри бро): ");
+                Write("Вкажіть вашу ціль по крокам: ");
                 double sumSteps = Convert.ToDouble(Console.ReadLine());
 
-                if (sumSteps == 0 || sumSteps < 0)
-                {
-                    ShowSeparator();
-                    Write("Серйозно? А в принципі, чого я дивуюся?");
-                    return;
-                }
+                    if (sumSteps == 0 || sumSteps < 0)
+                    {
+                        ShowSeparator();
+                        Write("Або ви не хочете ходити, або ви помилилися.");
+                        return;
+                    }
 
 
                 Write("Вкажіть скільки кроків ви вже пройшли: ");
                 double completeSteps = Convert.ToDouble(Console.ReadLine());
 
-                if (completeSteps < 0)
-                {
-                    ShowSeparator();
-                    Write("Ага, ага... А в принципі, що тут дивного?");
-                    return;
-                }
+                    if (completeSteps < 0)
+                    {
+                        ShowSeparator();
+                        Write("Ви не могли ходити від'ємно.");
+                        return;
+                    }
 
 
                 double summary = (completeSteps / sumSteps) * 100;
 
 
-                if (summary < 70)
-                {
-                    ShowSeparator();
-                    SummaryText("Давай рухайся", summary);
-                }
-
-                    else if (summary < 90)
+                    if (summary < 70)
                     {
                         ShowSeparator();
-                        SummaryText("Мета вже не за горами!", summary);
+                        SummaryText("Давай рухайся", summary);
                     }
 
-                        else if (summary >= 90 && summary <= 99)
+                        else if (summary < 90)
                         {
                             ShowSeparator();
-                            SummaryText("Давай! Ще трохи!", summary);
+                            SummaryText("Мета вже не за горами!", summary);
                         }
 
-                            else if (summary >= 100 && summary <= 199)
+                            else if (summary < 100)
                             {
                                 ShowSeparator();
-                                SummaryText("Ціль досягнута, молодчина! Тепер і поспати можна", summary);
+                                SummaryText("Давай! Ще трохи!", summary);
                             }
 
-                                else if (summary >= 200)
+                                else if (summary < 200)
                                 {
                                     ShowSeparator();
-                                    SummaryText("Та тобі на олімпійські ігри треба", summary);
+                                    SummaryText("Ціль досягнута, молодчина! Тепер і поспати можна", summary);
                                 }
-            }
-            catch(Exception ex)
-            {
-                ShowSeparator();
-                Write("ПОМИЛКА! Введено некоректне значення");
-                return;
-            }
+
+                                    else
+                                    {
+                                        ShowSeparator();
+                                        SummaryText("Та тобі на олімпійські ігри треба", summary);
+                                    }
+                }
+                catch(Exception ex)
+                {
+                    ShowSeparator();
+                    Write("!ПОМИЛКА! Введено некоректне значення. | " + "Причина: " + ex.Message);
+                    return;
+                }
             */
 
 
@@ -115,57 +110,67 @@ namespace Advance_1_DaynerPow
 
             //2.
 
-            /*double discount = 0;
-
-            ShowSeparator();
-            Write("Сума вашої покупки у грн:");
-            double purchase = Convert.ToDouble(Console.ReadLine());
-
-            if (purchase <= 0)
+            /*try
             {
-                Write("Помилка, Ви нічого не купили!");
-                return;
-            }
-            ShowSeparator();
-
-            Write("Ви маєте карту лояльності? 1 - так, 0 - ні");
-            string card = Console.ReadLine();
-            bool hasCard = card == "1";
-
-            if (hasCard)
-            {
-                discount += 3;
-                
-                if (purchase >= 20000)
-                {
-                    discount += 2;
-                }
+                double discount = 0;
 
                 ShowSeparator();
-                Write("Знижку за картою нараховано!");
+                Write("Сума вашої покупки у грн:");
+                double purchase = Convert.ToDouble(Console.ReadLine());
+
+                    if (purchase <= 0)
+                    {
+                        Write("Помилка, Ви нічого не купили!");
+                        return;
+                    }
+                    ShowSeparator();
+
+                Write("Ви маєте карту лояльності? 1 - так,  інше - ні");
+                string card = Console.ReadLine();
+                bool hasCard = card == "1";
+
+                    if (hasCard)
+                    {
+                        discount += 3;
+
+                            if (purchase >= 20000)
+                            {
+                                discount += 2;
+                            }
+
+                        ShowSeparator();
+                        Write("Знижку за картою нараховано!");
+                    }
+
+                    if (purchase >= 2000 && purchase < 10000)
+                    {
+                        discount += 1;
+                    }
+                        else if (purchase >= 10000)
+                        {
+                            discount += 5;
+                        }
+
+                Write("Загальна знижка:" + discount + "%");
+                ShowSeparator();
+
+
+                double cashBack = ((purchase * discount) / 100);
+
+                Write("Сума кешбеку " + cashBack + "грн");
+                ShowSeparator();
+
+                Write(purchase + " - " + cashBack);
+                Write("Загальна вартість: " + (purchase - cashBack) + "грн");
+
             }
-            
-            if(purchase >= 2000 && purchase < 10000)
+            catch (Exception ex) 
             {
-                discount += 1;
+                ShowSeparator();
+                Write("!ПОМИЛКА! Введено некоректне значення. | " + "Причина: " + ex.Message);
+                return;
             }
-                else if(purchase >= 10000)
-                {
-                    discount += 5;
-                }
-
-            Write("Загальна знижка:" + discount + "%");
-            ShowSeparator();
-
-            double cashBack = ((purchase * discount) / 100);
-
-            Write("Сума кешбеку " + cashBack + "грн");
-            ShowSeparator();
-
-            Write(purchase + " - " + cashBack);
-            Write("Загальна вартість: " + (purchase - cashBack) + "грн");
             */
-
 
 
 
@@ -176,35 +181,35 @@ namespace Advance_1_DaynerPow
                 Write("Введіть кількість спожитої електроенергії (кВ/год):");
                 double normal = Convert.ToDouble(Console.ReadLine());
 
-                if (normal < 0)
-                {
-                    Write("ПОМИЛКА! Електроенергія неможе бути від'ємною >:(");
-                    return;
-                }
-
-                if (normal <= 100)
-                {
-                    double result = normal * 1.44;
-                    ShowSeparator();
-                    ShowResult(result);
-                    return;
-                }
-                    else if (normal <= 600)
+                    if (normal < 0)
                     {
-                        Output(normal, 100, 1.68, 144);
+                        Write("ПОМИЛКА! Електроенергія неможе бути від'ємною >:(");
+                        return;
                     }
-                        else if (normal > 600)
+
+                    if (normal <= 100)
+                    {
+                        double result = normal * 1.44;
+                        ShowSeparator();
+                        ShowResult(result);
+                        return;
+                    }
+                        else if (normal <= 600)
                         {
-                            Output(normal, 600, 1.92, 984);
+                            Output(normal, 100, 1.68, 144);
                         }
+                            else if (normal > 600)
+                            {
+                                Output(normal, 600, 1.92, 984);
+                            }
             }
             catch (Exception ex)
             {
                 ShowSeparator();
-                Write("ПОМИЛКА! Введено некоректне значення");
+                Write("!ПОМИЛКА! Введено некоректне значення. | " + "Причина: " + ex.Message);
                 return;
-            }
-        */
+            }*/
+        
         }
     }
 }
